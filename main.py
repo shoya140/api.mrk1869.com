9e7b02a1c5654b75e315ec4aa17e54b0ff20d196
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2013-12-13 03:58:16 +0100

    mensa

--- main.py
+++ main.py
@@ -3,21 +3,40 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import pymongo
+import json
 
 from tornado.options import define, options
 define("port", default=6010, help="run on the given port", type=int)
 define("debug", default=0, help="1:watch in real time (debug mode)", type=bool)
 
+class Application(tornado.web.Application):
+    def __init__(self):
+        conn = pymongo.Connection("localhost", 27017)
+        self.db = conn.mensa
+
+        handlers=[
+            (r'/', IndexHandler),
+            (r'/mensa/v1/today/', MensaHandler)
+            ]
+        tornado.web.Application.__init__(self, handlers, debug=True)
+
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, World")
 
+class MensaHandler(tornado.web.RequestHandler):
+    def get(self):
+        db = self.application.db
+        item = db.mensa.menu.find_one({"date":"13.12.2013"})
+        self.write("{ \
+                date:"+item['date']+", \
+        menu1:{name:"+item['menu1']['name']+", image:"+item['menu1']['image']+"}, \
+        menu2:{name:"+item['menu2']['name']+", image:"+item['menu2']['image']+"} \
+        }")
+
 if __name__ == '__main__':
     tornado.options.parse_command_line()
-    app = tornado.web.Application(
-        debug=options.debug,
-        handlers=[(r'/', IndexHandler)],
-    )
-    http_server = tornado.httpserver.HTTPServer(app)
+    http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
